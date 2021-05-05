9435dae844c298b09beed86e6bfbd0f7396c3ba6
riyadhswe <riyadhswe@gmail.com>
2021-05-05 22:12:27 +0600

    your message

--- cvp/settings.py
+++ cvp/settings.py
@@ -130,6 +130,5 @@ MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
